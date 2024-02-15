330d4dd5c2bcbfcbc26a6028ff8d660869b049a6
Kamesh <kameshrg275@gmail.com>
2024-02-16 00:37:12 +0530

    some changes in navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -9,7 +9,7 @@ const Nav = () => {
   };
 
   return (
-    <nav className="bg-transparent text-white fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="bg-transparent text-white fixed w-full z-20 top-0 left-0 ">
       <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
         <span className="text-2xl font-semibold whitespace-nowrap dark:text-white">
           STRZZ
