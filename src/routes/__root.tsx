ea57e33d67e8b8b728a1756d63d2949124504014
kakashi1210 <desmondaditya@gmail.com>
2024-02-15 23:10:44 +0530

    feat: added navigation menu

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,18 +1,14 @@
 /* @global-imports */
-import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 /* @local-imports */
 import "./../index.css";
+import { NavigationMenuDemo } from "@/components/Navbar";
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="text-white flex items-center gap-2">
-        <Link to="/">Index</Link>
-        <Link to="/about">About</Link>
-        <Link to="/temp">temp</Link>
-      </div>
-      <hr />
+      <NavigationMenuDemo />
       <div className="text-white">This is Layout Page</div>
       <Outlet />
       <TanStackRouterDevtools />
