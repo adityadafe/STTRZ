5c84553f9eebefaa9e6329ed3baac7ee2f7876b0
Kamesh <kameshrg275@gmail.com>
2024-02-15 21:03:47 +0530

    feat : set up tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,7 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { useIndexStore } from "./store";
-
 function App() {
-  const count = useIndexStore((state) => state.count);
-  const increaseCount = useIndexStore((state) => state.increaseCount);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={increaseCount}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text-3xl font-bold underline text-blue-800 text-center">STRZZ</h1>
     </>
   );
 }
