355bed04b60d22107a2543e206ee9ed7e21008ad
Kamesh <kameshrg275@gmail.com>
2024-02-16 10:14:09 +0530

    feat : added header and navabr

--- src/routes/temp.tsx
+++ src/routes/temp.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 function TempPage() {
-  return <div className="text-white">Hello world</div>;
+  return <div className="text-white">Hello word</div>;
 }
 
 export const Route = createFileRoute("/temp")({
